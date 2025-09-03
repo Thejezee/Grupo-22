02322e6343a072fce5801e3bcfab48df646ce487
Mamani Agreda Josue Martin <jowebdesigc@gmail.com>
2025-09-03 11:38:32 -0400

    mi archivo saludo java

--- saludo.java
+++ saludo.java
@@ -1,10 +1,9 @@
-public class Saludo{
-    public static void main (String[] args){
-        System.out.println("Hola Equipo!")
-        System.out.println("Mamani Agreda Josue Martin")
-        System.out.println("Arteaga Yana Joaquin Octavio")
-        System.out.println("Coariti Colque Rosleny Mayra")
-        System.out.println("Tenorio Alanoca Ivan Rodrigo")
+public class Saludo {
+    public static void main(String[] args) {
+        System.out.println("Hola Equipo!");
+        System.out.println("Mamani Agreda Josue Martin");
+        System.out.println("Arteaga Yana Joaquin Octavio");
+        System.out.println("Coariti Colque Rosleny Mayra");
+        System.out.println("Tenorio Alanoca Ivan Rodrigo");
     }
-
 }
