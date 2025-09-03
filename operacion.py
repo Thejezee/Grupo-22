8a00d3046039c4c698ffa071ec9e647444f2ab68
Ivan-Tenorio-Alanoca <160164350+Ivan-Tenorio-Alanoca@users.noreply.github.com>
2025-09-03 11:50:10 -0400

    Mi archivo operacion python

---
+++ operacion.py
@@ -0,0 +1,9 @@
+# Operación simple
+numero1 = 5
+numero2 = 10
+suma = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es:", suma)
+print("1 Tenorio Alanoca Ivan Rodrigo")
+print("2 Arteaga Yana Joaquin Octavio")
+print("3 Mamani Agreda Josue Martin")
+print("4 Condori Colque Rosleny Mayra")
